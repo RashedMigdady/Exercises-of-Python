dd738c7a7ed1855fb4f57183c3cf71f3de1c3633
RashedMigdady <migdadyrashed@gmail.com>
2019-11-19 15:47:56 +0200

    Exercises_1

---
+++ Exercises_Python.py
@@ -0,0 +1,100 @@
+from datetime import datetime
+
+#Exercises 1 
+print("hello" , "\n" ,"\t","hello" ,"\n" ,"\t","\t", "hello")
+
+
+#Exercises 2
+print("Orange"*20)
+
+#Exercises 3
+"""
+1.0 , 2.8 , 3.0 ,4.2
+"""
+
+#Exercises 4
+num1 = int(input("Enter First Number :"))
+num2 = int(input("Enter First Number :"))
+
+product = num1 * num2
+print(product)
+
+#Exercise 5
+num1 = int(input("Enter First Number 1 :"))
+num2 = int(input("Enter First Number 2 :"))
+num3 = int(input("Enter First Number 3 :"))
+num4 = int(input("Enter First Number  4 :"))
+num5 = int(input("Enter First Number 5 :"))
+avg = num1 + num2 + num3 +num5 + num4 
+print("The Avareg is :",avg/5)
+
+#Exercise 6
+
+"""
+int
+float
+complex
+string
+boolean
+"""
+
+#Exercise 7
+"""
+Orange
+1
+error because X1 deffrence x1
+-10
+"""
+#Exercise 8
+"""
+Error because of there is a syntax Error (there is beginng digit)
+Error because of there is a syntax Error($)
+Error because of there is a syntax Error(space between the latter)
+Error because of there is a syntax Error(Print .. the correct is print)
+"""
+
+# Exercise 11
+"""
+Cheers , Mate!
+"""
+
+#Exercise 12
+age = int(input("Enter your age :"))
+now = datetime.now()
+year1 = now.year
+x = 100 - age
+print("You will be 100 in ", x + year1)
+
+#Exercise 13
+base = float(input("Enter the Base :"))
+height = float(input("Enter the Height :"))
+
+area = 0.5 *base * height
+print("the area of triangle is :" , area)
+
+#Exercise 14
+"""
+14
+8
+33
+3
+3.666
+2
+11
+11
+11.0
+2
+11^3
+11^3
+True
+False
+True
+
+"""
+
+
+
+
+
+
+
