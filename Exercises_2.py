af9b644059babb96fbbeb03e2c64536c4be8a3f1
RashedMigdady <migdadyrashed@gmail.com>
2019-11-20 16:21:54 +0200

    Exercises_2

---
+++ Exercises_2.py
@@ -0,0 +1,147 @@
+#Exercise 1
+'''
+try:
+  n = int(input("Please Enter any number: "))
+  m = int(input("Please Enter other number: "))
+  if n > m:
+    print(n)
+  elif m > n:
+    print(m)
+  else:
+    print("n is equal m")
+except ValueError:
+    print("Please enter correct value")
+    
+    
+# Exercise 2
+x = int(input("enter num "))
+
+for i in range(10):
+    print(x ," x ",i+1," =",x*(i+1))
+    
+#Exercise 3
+    
+for c in range(5):
+   for j in range(c):
+      print("*" ,end="") 
+   print("*")
+   
+for k in range(5 ,-1 ,-1):
+    for l in range(k):
+        print("*" ,end="") 
+    print("*")
+
+
+#Exercise 4
+    
+x
+y
+z
+b
+c
+
+
+#Exercise 5
+
+12
+15
+18
+21
+24
+
+
+#Exercise 6
+
+1
+2
+3
+
+
+#Exercise 7
+num = int(input("enter num: "))
+sum = 0
+
+for u in range(num+1):
+    sum = sum + u
+    
+print(sum)
+    
+#Exercise 8
+p = int(input("Enter any number :"))
+l = p%2
+if l == 0:
+    print("even")
+else:
+    print("Odd")
+    
+#Exercise 9
+    
+    
+#Exercise 10
+ 
+while True:
+    try:
+        z = int(input("enter any number : "))
+    except ValueError:
+        print("please enter just a number")
+    
+#Exercise 11
+
+
+Error Occurred and Handled
+
+
+'''
+for c in range(5):
+   for j in range(c):
+      print("*" ,end=" ") 
+   print("*")
+   
+for k in range(5 ,-1 ,-1):
+    for l in range(k):
+        print("*" ,end=" ") 
+    print("*")
+    
+
+
+   
+
+
+    
+    
+
+        
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
