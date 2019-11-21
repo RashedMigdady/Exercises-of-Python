fcfa332761a7313a12ad5cf9b38e71965a3bfc2e
RashedMigdady <migdadyrashed@gmail.com>
2019-11-21 15:08:21 +0200

    Exercises_3

---
+++ Exercises_3.py
@@ -0,0 +1,69 @@
+# Exercise 1
+list1 = [1 , 2 , 3 , 5 , 56]
+
+for x in list1:
+    print(x)
+    
+# Exercise 2
+print("The Sum = ",sum(list1))
+
+# Exercise 3
+print("The Max = " , list1)
+
+# Exercise 4
+a = [10 , 20 , 30 , 20 , 10 , 50 , 60 , 40 , 80 , 50 , 40]
+A = set(a)
+B = list(A)
+print(a)
+print(B)
+
+
+# Exercise 5
+empty =[]
+if a == empty:
+    print("is empty")
+else:
+    print("it is not empty")
+
+
+# Exercise 6
+tupl = ("Rashed" , 100)
+print(tupl[0] ,"\n", tupl[1])
+
+# Exercise 7
+num_set=set([0,1,2,3,4,5])
+for e in num_set:
+    print(e)
+
+# Exercise 8
+set_1 = {1 , 2 , 3 , 4}
+set_2 = {3 , 4 , 5 , 6 , 7}
+
+print(set_1 & set_2)
+
+# Exercise 9
+'''
+{"green","blue","yellow"}
+
+'''
+# Exercise 10
+'''
+6
+'''
+
+# Exercise 11
+'''
+
+{1:10 , 2:20 , 3:30 , 4:40 , 5:50 , 6:60}
+
+'''
+
+# Exercise 12
+"""
+e
+llo
+orl
+13
+hello, world!
+Jello, world!
+"""
